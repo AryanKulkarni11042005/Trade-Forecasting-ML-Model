45c6a526e33c1180a0c38ceffcc5badc63c18420
AryanKulkarni11042005 <aryan.kulkarni@vit.edu.in>
2026-04-22 19:57:52 +0530

    Launched to production

--- trade-forecast-frontend/src/services/api.ts
+++ trade-forecast-frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://trade-forecasting-ml-model.onrender.com',
   timeout: 15000,
 })
 
